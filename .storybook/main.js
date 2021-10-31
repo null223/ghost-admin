7519ffeda2e585db8185ca29edbe034ada977316
null223 <sumida0223@gmail.com>
2021-10-31 19:51:05 +0900

    enable: storybook

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/stories'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    };
+    return config;
+  },
+};
---
+++ .storybook/preview.js
@@ -0,0 +1,30 @@
+import { MemoryRouter as Router, Route } from 'react-router-dom';
+
+import Provider from '../src/components/Provider';
+
+export const parameters = {
+  actions: { argTypesRegex: '^(on|handle)[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /(Date|At)$/,
+    },
+  },
+  backgrounds: {
+    values: [
+      { name: 'white', value: '#fff' },
+      { name: 'gray', value: '#f3f3f6' },
+      { name: 'black', value: '#000' },
+    ],
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <Provider>
+      <Router>
+        <Story />
+      </Router>
+    </Provider>
+  ),
+];
