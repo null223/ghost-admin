27c8cfdfce9fb167cab4ce4f0112044608395513
null223 <sumida0223@gmail.com>
2021-11-02 00:53:48 +0900

    fix: config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  stories: ['../src/stories'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  stories: ['../src/**/*.stories.@(ts|tsx|js)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    // '@storybook/preset-create-react-app',
+  ],
   webpackFinal: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,15 @@
-const path = require('path')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   webpack: {
     alias: {
       '@': path.join(__dirname, 'src'),
     },
+    plugins: [
+      new webpack.ProvidePlugin({
+        React: 'react',
+      }),
+    ],
   },
 };
